9b896c5cb67e7ad05bd9478548e4bd000515424c
student1 <student1@jh-mbair-158-03.local>
2016-04-28 14:48:05 -0500

    initial

--- Messages/ViewController.swift
+++ Messages/ViewController.swift
@@ -1,25 +1,30 @@
-//
-//  ViewController.swift
-//  Messages
-//
-//  Created by student1 on 4/20/16.
-//  Copyright © 2016 JohnHerseyHighSchool. All rights reserved.
-//
-
 import UIKit
+import MessageUI
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, MFMessageComposeViewControllerDelegate {
+    
+    @IBOutlet weak var phoneNumber: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func sendText(sender: UIButton) {
+     if (MFMessageComposeViewController.canSendText()) {
+            let controller = MFMessageComposeViewController();
+            controller.body = "";
+            controller.recipients = ["(224)-456-7974"]
+            controller.messageComposeDelegate = self;
+            self.presentViewController(controller, animated: true, completion: nil);
+        }
     }
-
-
+    
+    func messageComposeViewController(controller: MFMessageComposeViewController!, didFinishWithResult result: MessageComposeResult) {
+        //... handle sms screen actions
+        self.dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    override func viewWillDisappear(animated: Bool) {
+        self.navigationController?.navigationBarHidden = false
+    }
 }
-
